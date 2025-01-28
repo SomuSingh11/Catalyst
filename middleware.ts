ec077c5e6df7fb02995ab6c6bc95835ae8935929
SOMU SINGH <somusingh0110@gmail.com>
2025-01-28 23:06:02 +0530

    Integrated authentication with Clerk, implemented SignIn and SignUp Page

---
+++ middleware.ts
@@ -0,0 +1,22 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+// Define public routes
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
+
+export default clerkMiddleware(async (auth, request) => {
+  // If the request is for a public route, allow access without authentication
+  if (isPublicRoute(request)) {
+    return;
+  }
+
+  // Protect all other routes
+  await auth.protect();
+});
+
+export const config = {
+  matcher: [
+    // Protect specific routes and handle API routes
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/(api|trpc)(.*)",
+  ],
+};
