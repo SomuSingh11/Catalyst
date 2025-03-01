3a669c0f095d6d3797a4b79b748d3abad3c80e1c
Somu <somusingh0110@gmail.com>
2025-03-01 17:19:19 +0530

    implement checkAnwer Route and fix schema

---
+++ app/api/quizzy/checkAnswer/route.ts
@@ -0,0 +1,71 @@
+import prisma from "@/lib/db";
+import { checkAnswerSchema } from "@/schemas/form/quiz";
+import { NextResponse } from "next/server";
+import { ZodError } from "zod";
+
+
+export async function POST(req : Request, res : Response){
+try {
+    const body = await req.json();
+    const parsedBody = checkAnswerSchema.parse(body);
+    const questionId = parsedBody.questionId;
+    const userAnswer = parsedBody.userAnswer;
+
+    const question = await prisma?.quizzyQuestion.findUnique({
+        where: {
+            id: questionId
+        }
+    });
+
+    if(!question){
+        return NextResponse.json(
+            {
+                error: "Question not found"
+            },
+            {
+                status: 404
+            }
+        )
+    }
+
+    await prisma?.quizzyQuestion.update({
+        where: {
+            id: questionId,
+        },
+        data: {
+            userAnswer: userAnswer
+        }
+    })
+
+    if(question.questionType === "mcq"){
+        const isCorrectAnswer = question.answer.toLocaleLowerCase().trim() === userAnswer.toLocaleLowerCase().trim();
+        await prisma?.quizzyQuestion.update({
+            where: {
+                id: questionId,
+            },
+            data:{
+                isCorrect: isCorrectAnswer
+            }
+        })
+
+        return NextResponse.json({
+            message: "Answer submitted successfully",
+            isCorrectAnswer: isCorrectAnswer,
+        }, {
+            status: 200
+        })
+    }
+
+} catch (error) {
+    if(error instanceof ZodError){
+        return NextResponse.json(
+            {
+                error: error.issues
+            },
+            {
+                status: 400
+            }
+        )
+    }
+}
+}
